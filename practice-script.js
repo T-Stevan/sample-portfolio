f382175c330610a9445a47edf26a7197d4e0a50f
TSM <trevor.stevan@gmail.com>
2025-07-31 17:56:10 +0300

    JavaScript Intro

---
+++ practice-script.js
@@ -0,0 +1,13 @@
+console.log("Hello from practice-script.js!");
+
+var projectTitle = "My Portfolio";
+console.log(projectTitle);it 
+
+const aboutMe = "Web developer passionate about building projects.";
+console.log(aboutMe);
+
+let skillsCount = 5;
+
+if (skillsCount > 3) {
+  console.log("You have lots of skills!");
+}
