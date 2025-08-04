9a61ad1cea77952ba670d46bbc8fc3ff19f71abd
TSM <trevor.stevan@gmail.com>
2025-08-04 17:17:17 +0300

    Javascript inclusion

--- practice-script.js
+++ practice-script.js
@@ -1,13 +1,14 @@
-console.log("Hello from practice-script.js!");
+// DOM Exploration
+console.log("=== DOM Exploration ===");
 
-var projectTitle = "My Portfolio";
-console.log(projectTitle);it 
+// Document information
+console.log("Document title:", document.title);
+console.log("Document URL:", window.location.href);
 
-const aboutMe = "Web developer passionate about building projects.";
-console.log(aboutMe);
+// Browser information
+console.log("Browser width:", window.innerWidth);
+console.log("Browser height:", window.innerHeight);
 
-let skillsCount = 5;
-
-if (skillsCount > 3) {
-  console.log("You have lots of skills!");
-}
+// Navigator information
+console.log("Browser:", navigator.userAgent);
+console.log("Platform:", navigator.platform);
